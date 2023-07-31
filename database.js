6e27e41b19ec844bb07e137339393e0ac0b984cb
prateek <prabar1621@gmail.com>
2023-07-31 17:51:15 +0530

    creating a table on connect

--- database.js
+++ database.js
@@ -1,10 +1,26 @@
 const mysql = require('mysql2')
-const dotenv=require('dotenv')
+const dotenv = require('dotenv')
 dotenv.config()
 
-module.exports = mysql.createConnection({
-    host: process.env.SQL_SERVER,
-    user: process.env.SQL_USER,
-    password: process.env.SQL_PASSWORD,
-    database: 'bitespeed'
-}).promise()
+const con = mysql.createConnection({ 
+    host: process.env.SQL_SERVER, 
+    user: process.env.SQL_USER, 
+    password: process.env.SQL_PASSWORD, 
+    database: 'bitespeed' 
+}).promise();
+     
+con.connect(function (err) { 
+    if (err) { 
+        console.log(err); 
+    } else { 
+        const sql = 'CREATE TABLE contact (id int, name varchar(20), email varchar(100), linkedId int, linkPrecedence varchar(20))'; 
+        con.query(sql, (err, result) => { 
+            if (err) { 
+                // console.log(err) 
+            } else { 
+                console.log("Table created"); 
+            } 
+        }) 
+    } 
+})
+module.exports = con;
